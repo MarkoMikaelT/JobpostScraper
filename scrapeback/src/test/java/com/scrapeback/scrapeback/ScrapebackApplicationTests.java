685e2e1198aea8a6a5a50c10d3bc08a34e8dda82
Marko Tiitto <marko.tiitto@hotmail.fi>
2023-01-23 17:50:38 +0200

    spring boot backend start

---
+++ scrapeback/src/test/java/com/scrapeback/scrapeback/ScrapebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapeback.scrapeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
